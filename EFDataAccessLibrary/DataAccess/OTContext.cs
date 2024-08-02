ddc74eceff1198d7e0348a50d8bd0e7240332774
Lamsses <androi1000d@gmail.com>
2024-08-02 20:14:58 +0300

    Add project files.

---
+++ EFDataAccessLibrary/DataAccess/OTContext.cs
@@ -0,0 +1,52 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.DataAccess;
+
+public class OTContext : DbContext
+{
+    public OTContext(DbContextOptions<OTContext> options) : base(options) { }
+
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+        modelBuilder.Entity<Employee>()
+            .HasMany(e => e.Orders)
+            .WithOne(o => o.Employee)
+            .HasForeignKey(o => o.EmployeeId);
+        modelBuilder.Entity<Customer>()
+            .HasMany(c => c.Orders)
+            .WithOne(o => o.Customer)
+            .HasForeignKey(o => o.CustomerId);
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.OrderItems)
+            .WithOne(oi => oi.Order)
+            .HasForeignKey(oi => oi.OrderId);
+        modelBuilder.Entity<OrderItem>().
+            HasOne(oi => oi.Product)
+            .WithMany(p => p.OrderItems)
+            .HasForeignKey(oi => oi.ProductId);
+
+         
+    }
+
+
+
+}
---
+++ EFDataAccessLibrary/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿
+namespace EFDataAccessLibrary.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string Recipt { get; set; }  
+    public decimal TotalAmount { get; set; }
+    public OrderStatus Status { get; set; }
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; } 
+
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; }
+
+    public ICollection<OrderItem> OrderItems { get; set; }
+}
+
+public enum OrderStatus
+{
+    Completed,
+    Refunded,
+}
